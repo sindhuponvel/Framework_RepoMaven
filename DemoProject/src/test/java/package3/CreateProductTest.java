7b7c42d58b8c3685f7ba2217fc5192808c2d1fe3
Chethan1306 <166906416+Chethan1306@users.noreply.github.com>
2024-05-31 18:04:50 +0530

    Added parameters  newly

--- DemoProject/src/test/java/package3/CreateProductTest.java
+++ DemoProject/src/test/java/package3/CreateProductTest.java
@@ -6,6 +6,15 @@ public class CreateProductTest {
 	@Test(groups = "Smoke Test")
 	public void createProduct()
 	{
+		String URL = System.getProperty("Url");
+		String BROWSER = System.getProperty("Browser");
+		String USERNAME = System.getProperty("Username");
+		String PASSWORD = System.getProperty("Password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+
 		System.out.println("Execute create Product");
 	}
 	@Test(groups = "Regression Test")
